4ba1f29070654ff2dbce2a89518c309e629d6a0c
massizelle <massizelle.boubadjou@se.univ-bejaia.dz>
2024-03-28 11:02:26 +0100

    exercice

--- script2.js
+++ script2.js
@@ -12,3 +12,10 @@ function test2 (){ //let , const are the scoope bloc
     console.log(i)
     }
     test2()
+/*
+creer une fonction format text qui a une chaine de car comme argument et effecue les opérations suivantes : 
+1- remplace tous les espaces par des -
+2- transorme toue la chaine en miniscule
+3- supprime tous les car indésirables : exmp ( Hello,devs we are in 2024) => (hellodevs-we-are-in-)
+indice : expréssions régulieres 
+*/
