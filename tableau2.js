ccaa286f320b66906ebe46aab8e3ffda72e50e80
massizelle <massizelle.boubadjou@se.univ-bejaia.dz>
2024-03-30 11:36:25 +0100

    edit

--- tableau2.js
+++ tableau2.js
@@ -1,17 +1,17 @@
-function test (){ //var is the scoope function
-for(  var i =0  ; i <4 ;  i++) {
-    console.log(i)
-}
-console.log(i)
-}
-test()
-function test2 (){ //let , const are the scoope bloc
-    for(  let i =0  ; i <4 ;  i++) {
-        console.log(i)
-    }
-    console.log(i)
-    }
-    test2()
+// function test (){ //var is the scoope function
+// for(  var i =0  ; i <4 ;  i++) {
+//     console.log(i)
+// }
+// console.log(i)
+// }
+// test()
+// function test2 (){ //let , const are the scoope bloc
+//     for(  let i =0  ; i <4 ;  i++) {
+//         console.log(i)
+//     }
+//     console.log(i)
+//     }
+//     test2()
 /*
 creer une fonction format text qui a une chaine de car comme argument et effecue les opérations suivantes : 
 1- remplace tous les espaces par des -
@@ -19,3 +19,12 @@ creer une fonction format text qui a une chaine de car comme argument et effecue
 3- supprime tous les car indésirables : exmp ( Hello,devs we are in 2024) => (hellodevs-we-are-in-)
 indice : expréssions régulieres 
 */
+function remplacee (chaine){
+    chaine = chaine.replace(/ /g, '-');
+    chaine = chaine.toLowerCase();
+    chaine = chaine.replace(/[^a-z-]/g, '');
+    return chaine;
+}
+let texte = "Hello, devs we are in 2024";
+let resultat = remplacee(texte);
+console.log(resultat);
